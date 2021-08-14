f82822b6e6f54900cab2efa6ef99d61c513e6232
robstrings97 <alvaro.robser@gmail.com>
2021-08-14 19:27:28 -0400

    test push

--- tarea_1/main.c
+++ tarea_1/main.c
@@ -15,9 +15,9 @@ int main(int argc, char *argv[])
     int key2_len = strlen(argv[3]);
     all_char_different(argv[2], key1_len);
     all_char_different(argv[3], key1_len);
-    
+
     // equal_length(argv[1],argv[2]);
-    /*obtenemos el largo de las key mediante strlen*/    
+    /*obtenemos el largo de las key mediante strlen*/
     // int len1 = strlen(argv[1]);
     // int len2 = strlen(argv[2]);
     // char *key1, *key2;
@@ -26,9 +26,10 @@ int main(int argc, char *argv[])
     // strcpy(key1,argv[1]);
     // strcpy(key2,argv[2]);
 
-    
     // printf("%c\n", *(key1+2));
     // printf("%s\n", key2);
 
     return 0;
 }
+
+//test.
