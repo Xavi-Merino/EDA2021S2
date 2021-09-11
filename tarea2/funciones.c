7c3998be18b84eaac3fc4efd9a5486bf221b1a77
robstrings97 <alvaro.robser@gmail.com>
2021-09-11 20:13:30 -0300

    hechas las validaciones e interrupciones programa

--- tarea2/funciones.c
+++ tarea2/funciones.c
@@ -12,15 +12,15 @@ persona* leer_archivo(char* filename, int* N) {
   while (fgets(line, 999, filelist) != NULL) {
     count++;
   }
-  count--; // para que no cuente el titulo 
+  count--;  // para que no cuente el titulo
   fclose(filelist);
   *N = count;
   personas = (persona*)malloc(count * sizeof(persona));
 
   filelist = fopen(filename, "r");
   count = 0;
-  fgets(line,999,filelist);
-  
+  fgets(line, 999, filelist);
+
   while (fgets(line, 999, filelist) != NULL) {
     strcpy(p.name, strtok(line, ","));
     p.danger_category = atoi(strtok(NULL, ","));
@@ -31,66 +31,60 @@ persona* leer_archivo(char* filename, int* N) {
   return personas;
 }
 
-void swap(persona *src, persona* dst){
+void swap(persona* src, persona* dst) {
   persona tmp = *src;
   *src = *dst;
   *dst = tmp;
 }
 
-void show_personas(persona* personas, int cant_personas) {
-  for (int i = 0; i < cant_personas; i++) {
+void show_personas(persona* personas, int cant_personas, int N_output) {
+  for (int i = 0; i < N_output; i++) {
     printf("%s, %i, %f\n", personas[i].name, personas[i].danger_category,
            personas[i].attack_prob);
   }
 }
 
-
-static void Merge(persona *arr, int low, int mid, int high) //ordena danger_category
+static void Merge(persona* arr, int low, int mid,
+                  int high)  // ordena danger_category
 {
   int mergedSize = high - low + 1;
-  persona *temp = (persona *)malloc(mergedSize * sizeof(persona));
+  persona* temp = (persona*)malloc(mergedSize * sizeof(persona));
   int mergePos = 0;
   int leftPos = low;
   int rightPos = mid + 1;
-  while (leftPos <= mid && rightPos <= high)  //aca sucede la magia
+  while (leftPos <= mid && rightPos <= high)  // aca sucede la magia
   {
-    if (arr[leftPos].danger_category < arr[rightPos].danger_category) //1ro ordenamos por categoria
+    if (arr[leftPos].danger_category <
+        arr[rightPos].danger_category)  // 1ro ordenamos por categoria
     {
       temp[mergePos++] = arr[leftPos++];
-    }
-    else if((arr[leftPos].danger_category == arr[rightPos].danger_category)
-            && arr[leftPos].attack_prob < arr[rightPos].attack_prob)//2do por attack_prob
+    } else if ((arr[leftPos].danger_category ==
+                arr[rightPos].danger_category) &&
+               arr[leftPos].attack_prob <
+                   arr[rightPos].attack_prob)  // 2do por attack_prob
     {
-      temp[mergePos++] = arr[leftPos++];        
-    }
-    else
-    {
+      temp[mergePos++] = arr[leftPos++];
+    } else {
       temp[mergePos++] = arr[rightPos++];
     }
   }
 
-  while (leftPos <= mid)
-  {
+  while (leftPos <= mid) {
     temp[mergePos++] = arr[leftPos++];
   }
 
-  while (rightPos <= high)
-  {
+  while (rightPos <= high) {
     temp[mergePos++] = arr[rightPos++];
   }
 
-
-
-  for (mergePos = 0; mergePos < mergedSize; ++mergePos) 
+  for (mergePos = 0; mergePos < mergedSize; ++mergePos)
     arr[low + mergePos] = temp[mergePos];
 
-    free(temp);
+  free(temp);
 }
 
-void MergeSort(persona *arr, int low, int high)
-{
-  if (low < high)
-  {
+void MergeSort(persona* arr, int low, int high) {
+  if (low < high) {
     int mid = (low + high) / 2;
 
     MergeSort(arr, low, mid);
@@ -100,40 +94,30 @@ void MergeSort(persona *arr, int low, int high)
   }
 }
 
-void swap_cat12_0prob(persona* arr, int cant_personas)
-{
+void swap_cat12_0prob(persona* arr, int cant_personas) {
   persona tmp;
   int cant_cat1 = 0, cant_cat2 = 0;
 
-  for(int i = 0; i < cant_personas; i++)
-  {
-    if(arr[i].danger_category == 1)
-    { 
+  for (int i = 0; i < cant_personas; i++) {
+    if (arr[i].danger_category == 1) {
       cant_cat1++;
-    }
-    else if(arr[i].danger_category == 2)
-    {
+    } else if (arr[i].danger_category == 2) {
       cant_cat2++;
     }
   }
   printf("%d %d", cant_cat1, cant_cat2);
-  for(int i = 0 ; i < cant_cat1 - 1 ; i++)
-  {
-    if(arr[i].attack_prob == 0 && i != cant_cat1)
-    {
+  for (int i = 0; i < cant_cat1 - 1; i++) {
+    if (arr[i].attack_prob == 0 && i != cant_cat1) {
       tmp = arr[i];
     }
-    arr[i] = arr[i+1];
-    arr[cant_cat1-1] = tmp;
+    arr[i] = arr[i + 1];
+    arr[cant_cat1 - 1] = tmp;
   }
-  for(int i = cant_cat1 ; i < cant_cat2- 1 ; i++)
-  {
-    if(arr[i].attack_prob == 0 && i != cant_cat2)
-    {
+  for (int i = cant_cat1; i < cant_cat2 - 1; i++) {
+    if (arr[i].attack_prob == 0 && i != cant_cat2) {
       tmp = arr[i];
     }
-    arr[i] = arr[i+1];
-    arr[cant_cat2-1] = tmp;
+    arr[i] = arr[i + 1];
+    arr[cant_cat2 - 1] = tmp;
   }
 }
-
--- tarea2/main.c
+++ tarea2/main.c
@@ -5,19 +5,54 @@
 #include "funciones.h"
 
 int main(int argc, char *argv[]) {
+  if (argc != 4) {
+    printf("%s\n", "¡ERROR!, el programa solo recibe 3 argumentos.");
+    exit(-1);
+  }
+
+  if (strcmp(argv[1], argv[2]) == 0) {
+    printf("%s\n",
+           "¡ERROR!, el los nombres de los archivos de entrada y salida deben "
+           "ser diferentes");
+    exit(-1);
+  }
+
   int cant_personas;
   int cant_personas_out = atoi(argv[2]);
-  /*prueba de que lee los nombres y su info*/
+
   persona *p = leer_archivo(argv[1], &cant_personas);
-  show_personas(p, cant_personas);
+
+  if (cant_personas_out > cant_personas) {
+    printf("%s\n",
+           "¡ERROR!, la cantidad de personas solicitadas excede la cantidad de "
+           "personas que hay en el archivo");
+    exit(-1);
+  }
+
+  if (cant_personas_out < 0) {
+    printf(
+        "%s\n",
+        "¡ERROR! No contaban con nuestra astucia...Debe ingresar un número no "
+        "negativo como cantidad de "
+        "personas");
+    exit(-1);
+  }
+
+  if (cant_personas_out == 0) {
+    printf("%s\n",
+           "¡Error! La idea es mostrar una cantidad de personas, y el 0 tiene "
+           "valor nulo...Use un número entero ");
+    exit(-1);
+  }
+  /*prueba de que lee los nombres y su info*/
+  show_personas(p, cant_personas, cant_personas_out);
   // swap(p,(p+1));
   // printf("\n\n");
   // show_personas(p, cant_personas);
 
-
-  MergeSort(p, 0, cant_personas - 1);
-  printf("\n\n");
-  show_personas(p, cant_personas);
+  // MergeSort(p, 0, cant_personas - 1);
+  // printf("\n\n");
+  // show_personas(p, cant_personas);
   // swap_cat12_0prob(p,cant_personas);
   // printf("\n\n");
   // show_personas(p, cant_personas);
